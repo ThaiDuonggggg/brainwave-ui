b8b967cb2f0edb79212867954268e84814344f47
ThaiDuonggggg <hongdyong990@gmail.com>
2024-06-26 00:20:24 +0700

    2. Add file assets and complete Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import ButtonGradient from './assets/svg/ButtonGradient';
 
 import './App.css';
 import Button from './components/Button';
+import Header from './components/Header';
 
 function App() {
     return (
         <>
-            <h1 className="">Hello Word</h1>
             <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-                <Button className="mt-10" href="#login">
-                    some thing
-                </Button>
+                <Header />
             </div>
             <ButtonGradient />
         </>
