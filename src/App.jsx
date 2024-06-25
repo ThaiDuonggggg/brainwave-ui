1f102750b6e0aaff90dc71d50366df03ba5a2ef8
ThaiDuonggggg <hongdyong990@gmail.com>
2024-06-25 13:54:40 +0700

    set up Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ButtonGradient from './assets/svg/ButtonGradient';
+
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1 className="">Hello Word</h1>
+            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
+            <ButtonGradient />
+        </>
+    );
+}
+
+export default App;
