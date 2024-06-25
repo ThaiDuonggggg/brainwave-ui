ca4b6b16fe3aeb7f558822f5998244e9a9c68617
ThaiDuonggggg <hongdyong990@gmail.com>
2024-06-25 15:21:35 +0700

    1. Create components Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
 
 import './App.css';
+import Button from './components/Button';
 
 function App() {
     return (
         <>
             <h1 className="">Hello Word</h1>
-            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
+            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+                <Button className="mt-10" href="#login">
+                    some thing
+                </Button>
+            </div>
             <ButtonGradient />
         </>
     );
