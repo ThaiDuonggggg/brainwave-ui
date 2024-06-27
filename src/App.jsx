c1df28589f70f4014c91f05a713d61a304045446
ThaiDuonggggg <hongdyong990@gmail.com>
2024-06-27 17:58:19 +0700

    6. Complete Bento Box Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
 import Collaboration from './components/Collaboration';
+import Services from './components/Services';
 
 import './App.css';
 
@@ -14,6 +15,7 @@ function App() {
                 <Hero />
                 <Benefits />
                 <Collaboration />
+                <Services />
             </div>
             <ButtonGradient />
         </>
--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,9 +1,10 @@
 import PropTypes from 'prop-types';
 
-const Heading = ({ className, title }) => {
+const Heading = ({ className, title, text }) => {
     return (
         <div className={`${className} max-w-[50rem] mx-auto mb-12 lg:mb-20`}>
             {title && <h2 className="h2">{title}</h2>}
+            {text && <p className="body-2 mt-4 text-n-4">{text}</p>}
         </div>
     );
 };
@@ -11,6 +12,7 @@ const Heading = ({ className, title }) => {
 Heading.propTypes = {
     className: PropTypes.string,
     title: PropTypes.string,
+    text: PropTypes.string,
 };
 
 export default Heading;
