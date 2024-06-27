8ed37bad8ad2e69fa291dbb84eff768c0ab41f49
ThaiDuonggggg <hongdyong990@gmail.com>
2024-06-27 15:54:19 +0700

    5. Complete Collaboration

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import ButtonGradient from './assets/svg/ButtonGradient';
-
-import './App.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Benefits from './components/Benefits';
+import Collaboration from './components/Collaboration';
+
+import './App.css';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Header />
                 <Hero />
                 <Benefits />
+                <Collaboration />
             </div>
             <ButtonGradient />
         </>
